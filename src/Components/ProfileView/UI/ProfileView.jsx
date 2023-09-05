94441c2439922f3c3779199a4f247e0aa90276e0
Sourav0809 <spathak7431@gmail.com>
2023-09-06 01:42:00 +0530

    FE : premium features work progress

--- src/Components/ProfileView/UI/ProfileView.jsx
+++ src/Components/ProfileView/UI/ProfileView.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import Button from "../../UI/Button/Button";
-import { useContext } from "react";
-import authContext from "../../../Context/AuthContext/authContext";
-import userProfileCtx from "../../../Context/UserProfile/userProfileCtx";
+import { AiFillSmile } from "react-icons/ai";
+import { BiSolidUserCircle } from "react-icons/bi";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 const ProfileView = () => {
-  const authCtx = useContext(authContext);
-  const userCtx = useContext(userProfileCtx);
+  const userProfile = useSelector((state) => state.userProfile.userInfo);
+
+  console.log(userProfile);
   const verifyEmailHandeler = async () => {
     const idToken = localStorage.getItem("idToken");
     try {
@@ -20,25 +21,55 @@ const ProfileView = () => {
     }
   };
 
-  // if user want to log out
-
-  const logOutHandeler = () => {
-    localStorage.removeItem("idToken");
-    authCtx.setUserLoggedIn(false);
-    authCtx.setIdToken("");
-    userCtx.setUserInfo("");
-  };
-
   return (
-    <div>
-      <div className=" mt-36 text-center text-5xl">Your Profile</div>;
-      <div className=" flex justify-center items-center gap-2">
-        <Button>
-          <span onClick={verifyEmailHandeler}>Verify Email</span>
-        </Button>
-        <Button>
-          <span onClick={logOutHandeler}>Log Out</span>
-        </Button>
+    <div className=" pl-[3.4rem]">
+      <div className=" w-[95%] p-3  md:w-[45rem] border m-auto mt-20 text-center shadow-md">
+        <h2 className="text-4xl text-blue-950 font-popins font-bold mt-6">
+          Your Account
+        </h2>
+
+        <div className=" flex justify-center item-center mt-2">
+          <BiSolidUserCircle className=" text-[8rem] text-blue-900" />
+        </div>
+
+        <div className=" w-full p-5 flex flex-col gap-2">
+          <input
+            disabled
+            p-1
+            value={"Sourav Pathak"}
+            className="w-full p-2 rounded-sm bg-[#9bddc2]"
+          />
+          <input
+            disabled
+            p-1
+            value={"spathak7431@gmail.com"}
+            className="w-full p-2 rounded-sm bg-[#9bddc2]"
+          />
+          <input
+            disabled
+            p-1
+            value={"6294543902"}
+            className="w-full p-2 rounded-sm bg-[#9bddc2]"
+          />
+          <input
+            disabled
+            p-1
+            value={"**********"}
+            className="w-full p-2 rounded-sm  bg-[#9bddc2]"
+          />
+          <div className=" flex justify-center item-center gap-2">
+            <button className="mt-3 py-2 bg-[#565be9d0] md:w-[100%] w-full font-semibold text-white rounded-sm">
+              Verify Email
+            </button>
+          </div>
+
+          <div className=" mb-7">
+            <h1 className=" text-base font-semibold">
+              Verify your account to use 100% of our app
+            </h1>
+            <button className=" mt-2 font-bold">Unlock VIP</button>
+          </div>
+        </div>
       </div>
     </div>
   );
